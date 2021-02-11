99d2593c125999074ffedd585b93c81176316684
Nikolai Kiselev <kisnikn@mail.ru>
2021-02-11 20:54:00 +0300

    functions equalBalance and login were added

--- src/test/java/test/PageObjectTest.java
+++ src/test/java/test/PageObjectTest.java
@@ -10,23 +10,31 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PageObjectTest {
+    LoginPage loginPage;
+    DataHelper.AuthInfo authInfo;
+    VerificationPage verificationPage;
+    DataHelper.VerificationCode verificationCode;
+    DashboardPage dashBoardPage;
+    int balanceOne;
+    int balanceTwo;
 
     @BeforeEach
     void openWebSite() {
         open("http://localhost:7777");
     }
 
+    void login() {
+        loginPage = new LoginPage();
+        authInfo = DataHelper.getAuthInfo();
+        verificationPage = loginPage.validLogin(authInfo);
+        verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        dashBoardPage = verificationPage.validVerify(verificationCode);
+    }
 
+    void equalBalance() {
 
-    @Test
-    void moneyTransferFirstCardTest() {
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        val dashBoardPage = verificationPage.validVerify(verificationCode);
-        int balanceOne = dashBoardPage.getFirstCardBalance();
-        int balanceTwo = dashBoardPage.getSecondCardBalance();
+        balanceOne = dashBoardPage.getFirstCardBalance();
+        balanceTwo = dashBoardPage.getSecondCardBalance();
         //уравнивание баланса
         if (balanceOne > balanceTwo) {
             val dashBoardTransferPage = dashBoardPage.makeTransferSecond();
@@ -40,8 +48,14 @@ public class PageObjectTest {
         }
         balanceOne = dashBoardPage.getFirstCardBalance();
         balanceTwo = dashBoardPage.getSecondCardBalance();
+    }
+
+    @Test
+    void moneyTransferFirstCardTest() {
+        login();
+        equalBalance();
         val dashBoardTransferPage = dashBoardPage.makeTransferFirst();
-        val newDashboardPage = dashBoardTransferPage.transferCard(DataHelper
+        dashBoardTransferPage.transferCard(DataHelper
                 .getMoneyTransfer("500", "5559000000000002"));
         int expectedOne = balanceOne + 500;
         int actualOne = dashBoardPage.getFirstCardBalance();
@@ -55,28 +69,10 @@ public class PageObjectTest {
 
     @Test
     void moneyTransferSecondCardTest() {
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        val dashBoardPage = verificationPage.validVerify(verificationCode);
-        int balanceOne = dashBoardPage.getFirstCardBalance();
-        int balanceTwo = dashBoardPage.getSecondCardBalance();
-        //уравнивание баланса
-        if (balanceOne > balanceTwo) {
-            val dashBoardTransferPage = dashBoardPage.makeTransferSecond();
-            val newDashboardPage = dashBoardTransferPage.transferCard(DataHelper
-                    .getMoneyTransfer(String.valueOf((balanceOne-balanceTwo)/2), "5559000000000001"));
-        }
-        if (balanceOne < balanceTwo) {
-            val dashBoardTransferPage = dashBoardPage.makeTransferFirst();
-            val newDashboardPage = dashBoardTransferPage.transferCard(DataHelper
-                    .getMoneyTransfer(String.valueOf((balanceTwo-balanceOne)/2), "5559000000000002"));
-        }
-        balanceOne = dashBoardPage.getFirstCardBalance();
-        balanceTwo = dashBoardPage.getSecondCardBalance();
+        login();
+        equalBalance();
         val dashBoardTransferPage = dashBoardPage.makeTransferSecond();
-        val newDashboardPage = dashBoardTransferPage.transferCard(DataHelper
+        dashBoardTransferPage.transferCard(DataHelper
                 .getMoneyTransfer("600", "5559000000000001"));
         int expectedTwo = balanceTwo + 600;
         int actualTwo = dashBoardPage.getSecondCardBalance();
@@ -89,28 +85,10 @@ public class PageObjectTest {
 
     @Test
     void moneyTransferMoreThanLimit () {
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        val dashBoardPage = verificationPage.validVerify(verificationCode);
-        int balanceOne = dashBoardPage.getFirstCardBalance();
-        int balanceTwo = dashBoardPage.getSecondCardBalance();
-        //уравнивание баланса
-        if (balanceOne > balanceTwo) {
-            val dashBoardTransferPage = dashBoardPage.makeTransferSecond();
-            val newDashboardPage = dashBoardTransferPage.transferCard(DataHelper
-                    .getMoneyTransfer(String.valueOf((balanceOne-balanceTwo)/2), "5559000000000001"));
-        }
-        if (balanceOne < balanceTwo) {
-            val dashBoardTransferPage = dashBoardPage.makeTransferFirst();
-            val newDashboardPage = dashBoardTransferPage.transferCard(DataHelper
-                    .getMoneyTransfer(String.valueOf((balanceTwo-balanceOne)/2), "5559000000000002"));
-        }
-        balanceOne = dashBoardPage.getFirstCardBalance();
-        balanceTwo = dashBoardPage.getSecondCardBalance();
+        login();
+        equalBalance();
         val dashBoardTransferPage = dashBoardPage.makeTransferFirst();
-        val newDashboardPage = dashBoardTransferPage.transferCard(DataHelper
+        dashBoardTransferPage.transferCard(DataHelper
                 .getMoneyTransfer(String.valueOf(balanceOne + 500), "5559000000000002"));
         int expectedOne = balanceOne*2;
         int actualOne = dashBoardPage.getFirstCardBalance();
